86beb1fc412203656771c78f1547aafa9320d293
Urvashi Dhingra <dhingraurvashi99@gmail.com>
2020-07-01 21:14:51 +0530

    Second game

---
+++ Assets/scripts/Driver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    const float MoveUnitsPerSecond = 5;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        if (horizontalInput != 0) 
+        {
+            Vector3 position = transform.position;
+            position.x += horizontalInput * MoveUnitsPerSecond * Time.deltaTime;
+            transform.position = position;
+        }
+        if (verticalInput != 0)
+        {
+            Vector3 position = transform.position;
+            position.y += verticalInput * MoveUnitsPerSecond * Time.deltaTime;
+            transform.position = position;
+        }
+    }
+}
